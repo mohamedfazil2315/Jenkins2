1a8aaeb22dec117e5a967a14f68ee2723aa0487c
mohamedfazil2315 <mohamedfazil2315@gmail.com>
2026-02-06 12:36:29 +0530

    Fix print statement to show subtraction instead of multiplication

--- python.py
+++ python.py
@@ -4,4 +4,4 @@ import sys
 print("Jenkins build using parameters !")
 X_VALUE = int(sys.argv[1])
 Y_VALUE = int(sys.argv[2])
-print("Multiply= ", (X_VALUE + Y_VALUE))
+print("Subract= ", (X_VALUE + Y_VALUE))
